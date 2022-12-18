621d0f813e5b7946e7a100707fdf1ffe12b14d57
jiangchang <jiangchang@gmail.com>
2022-12-18 03:00:24 -0500

    add key board

--- app/src/main/java/com/jchip/wear/country/city/CountryContentActivity.java
+++ app/src/main/java/com/jchip/wear/country/city/CountryContentActivity.java
@@ -1,21 +1,14 @@
 package com.jchip.wear.country.city;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.InputFilter;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-
-import android.app.Activity;
-
-
-import com.jchip.wear.country.city.util.CountryUtility;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +19,7 @@ public class CountryContentActivity extends Activity {
     private Map<String, String[]> info;
     private List<String> gridInfo;
 
-    private EditText searchText;
+    private TextView searchText;
     private RecyclerView gridView;
     private RecyclerView.ItemDecoration itemDecoration;
 
@@ -42,9 +35,24 @@ public class CountryContentActivity extends Activity {
         this.info = CountryViewHelper.getISOInfo();
         this.gridInfo = new ArrayList<>(this.info.keySet());
 
-        this.initSearchText();
+        this.findViewById(R.id.country_title).setOnClickListener((v) -> loadKeyboard());
 
+        this.searchText = this.findViewById(R.id.country_search);
+
+        Intent intent = this.getIntent();
+        String searchText = intent.getStringExtra("searchText");
+        this.searchText.setText(searchText);
+        this.findViewById(R.id.country_hint).setVisibility(searchText == null || searchText.isEmpty() ? View.VISIBLE : View.INVISIBLE);
+        
         this.onSearch();
+    }
+
+    private void loadKeyboard() {
+        TextView searchText = this.findViewById(R.id.country_search);
+        Intent intent = new Intent(this, SearchKeyboardActivity.class);
+        intent.putExtra("text", searchText.getText().toString());
+        intent.putExtra("class", this.getClass().getName());
+        startActivity(intent);
     }
 
     private void refreshGridView() {
@@ -55,25 +63,23 @@ public class CountryContentActivity extends Activity {
     }
 
     private void initSearchText() {
-        searchText = findViewById(R.id.country_search);
-        searchText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                int maxLength = CountryViewHelper.getInputCount(CountryUtility.isPortrait(CountryContentActivity.this));
-                searchText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(maxLength)});
-                CountryUtility.runOnUiWorker(CountryContentActivity.this, R.id.grid_processing, () -> {
-                    onSearch();
-                });
-            }
-        });
+//        searchText = findViewById(R.id.country_search);
+//        searchText.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//            }
+//
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                //CountryUtility.runOnUiWorker(CountryContentActivity.this, R.id.grid_processing, () -> {
+//                    onSearch();
+//               // });
+//            }
+//        });
 //        searchText.setOnEditorActionListener((textView, actionId, keyEvent) -> {
 //            if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_GO || actionId == EditorInfo.IME_ACTION_NEXT) {
 //                textView.clearFocus();
@@ -87,8 +93,9 @@ public class CountryContentActivity extends Activity {
 
     private void onSearch() {
         String searchText = this.searchText.getText().toString().trim().toUpperCase();
+        Log.d("xx", "searchText=" + searchText);
         this.gridInfo = CountryViewHelper.searchCountryInfo(this, this.info, searchText);
-       Log.d("xx", "info size=" + this.gridInfo.size());
+        Log.d("xx", "info size=" + this.gridInfo.size());
         CountryViewHelper.sortCountryInfo(this, this.info, this.gridInfo, 0);
         refreshGridView();
     }
---
+++ app/src/main/java/com/jchip/wear/country/city/SearchKeyboardActivity.java
@@ -0,0 +1,62 @@
+package com.jchip.wear.country.city;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SearchKeyboardActivity extends Activity {
+    private String className;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_keyboard_activity);
+
+        Intent intent = this.getIntent();
+        String text = intent.getStringExtra("text");
+        this.className = intent.getStringExtra("class");
+
+        EditText textView = this.findViewById(R.id.input_text);
+        textView.setText(text);
+
+        this.findViewById(R.id.key_done).setOnClickListener((v) -> this.onSubmit());
+        this.findViewById(R.id.key_back).setOnClickListener((v) -> this.finish());
+
+        this.initKeyboard();
+    }
+
+    private void onKeyboard(String key) {
+        EditText textView = this.findViewById(R.id.input_text);
+        String text = textView.getText().toString();
+        textView.setText(text + key);
+    }
+
+    private void onSubmit()  {
+        try {
+            TextView searchText = this.findViewById(R.id.input_text);
+            Intent intent = new Intent(this, Class.forName(this.className));
+            intent.putExtra("searchText", searchText.getText().toString());
+            startActivity(intent);
+        } catch (Exception ignore) {
+        }
+    }
+
+    private void initKeyboard() {
+        String[] keys = {
+                "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+                "A", "S", "D", "F", "G", "H", "J", "K", "L",
+                "Z", "X", "C", "V", "B", "N", "M",
+        };
+
+        for (int index = 0; index < keys.length; index++) {
+            int id = this.getResources().getIdentifier("key_" + index, "id",
+                    this.getPackageName());
+            TextView keyView = this.findViewById(id);
+            keyView.setText(keys[index]);
+            int finalIndex = index;
+            keyView.setOnClickListener((v) -> this.onKeyboard(keys[finalIndex]));
+        }
+    }
+}
